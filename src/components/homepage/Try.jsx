121e3e0be37765b770832ece47de4fd2f3320b8a
tehreem71 <f190143@cfd.nu.edu.pk>
2022-12-12 02:48:55 +0500

    adding carousel

--- src/components/homepage/Try.jsx
+++ src/components/homepage/Try.jsx
@@ -1,22 +1,44 @@
 import React from 'react'
 import { useEffect } from "react";
 import "./Try.css";
+import Carousel from 'react-bootstrap/Carousel';
 import * as THREE from 'three';
 import { color } from '@mui/system';
-import logo from "../../assets/Picture1.png";
+import icit from "../../assets/icit.jpg";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {
+    MDBCarousel,
+    MDBCarouselItem,
+  } from 'mdb-react-ui-kit';
 
 const Try = () => {
 
     return (
         <div className="home">
             
-            <div className="homeCanvas">
-                
-            </div>
+                <MDBCarousel showControls fade>
+                    <MDBCarouselItem
+                        className='w-100 d-block'
+                        itemId={1}
+                        src='https://mdbootstrap.com/img/new/slides/041.jpg'
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100 d-block'
+                        itemId={2}
+                        src='https://mdbootstrap.com/img/new/slides/042.jpg'
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100 d-block'
+                        itemId={3}
+                        src='https://mdbootstrap.com/img/new/slides/043.jpg'
+                        alt='...'
+                    />
+                    </MDBCarousel>
             <div className="homeContainer">
             <h3>THIS IS MY NEXT CANVAS</h3>
             <p>lorem ipsum jaskakhjkjshdkajjjjjjjjjjjjdhaknsk</p>
