f87cdb11f0b87c15d6887747723ee66380ec6161
tehreem71 <f190143@cfd.nu.edu.pk>
2022-12-29 19:43:54 +0500

    landing divs complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {ReactNavbar} from "overlay-navbar"
-import logo from "../../assets/Picture1.png";
+import logoNav from "../../assets/logo-copy.png";
 import { FaUserAlt } from "react-icons/fa";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -10,9 +10,10 @@ import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 const Header = () => {
-  return (<Navbar collapseOnSelect expand="lg" variant="light" className="color-nav">
-  <Container className="color-nav">
-      <Navbar.Brand href="#home"><img src={logo} width="100" height="100"></img></Navbar.Brand>
+  return (
+    <Navbar collapseOnSelect expand="sm" variant="light" className="navbar">
+        <Container className="color-nav">
+      <Navbar.Brand href="/try"><img src={logoNav}></img></Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="me-auto">
@@ -20,17 +21,13 @@ const Header = () => {
       </Nav>
       <Nav >
       <Nav.Link href="/try">Home</Nav.Link>
-          <Nav.Link href="/login">Login</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+          <Nav.Link href="/login">About Us</Nav.Link>
+          <NavDropdown title="More Options" id="collasible-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Take a Tour</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">
-              Another action
+              Contact
           </NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">
-              Separated link
-          </NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Add a complain</NavDropdown.Item>
           </NavDropdown>
           <Button variant="outline-info" size='sm' style={{height: '40px', width : '100px' }} href="./login">Login</Button>
           <Button variant="secondary" size='sm' style={{height: '40px', width : '100px'}} href="./register">Sign Up</Button>
--- src/components/homepage/Try.jsx
+++ src/components/homepage/Try.jsx
@@ -20,13 +20,53 @@ import vid from "../../assets/vid.mp4";
 import party from "../../assets/party.mp4";
 import cover5 from "../../assets/cover5.png";
 import cover6 from "../../assets/new.png";
-import {
-    MDBCarousel,
-    MDBCarouselItem,
-} from 'mdb-react-ui-kit';
+import  { useState } from "react"
+import axios from "axios"
+import { useHistory } from "react-router-dom"
+//import Picture1 from './assets/Picture1.png'
+import logo from "../../assets/Picture1.png";
 
-const Try = () => {
+const Try = ({ setLoginUser }) => {
+    const history = useHistory()
+    //asd
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+    })
 
+    const handleChange = e => {
+        const { name, value } = e.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
+    const login = () => {
+        axios.post("http://localhost:9002/login", user)
+            .then(res => {
+                //alert(res.data.message)
+                setLoginUser(res.data.user)
+                if(res.data.message === "Teacher")
+                {
+                    history.push("/teacherPortal")
+                }
+                else if (res.data.message === "Mentor")
+                {
+                    history.push("/mentorPortal")
+                }
+                else if (res.data.message === "Photography Head")
+                {
+                    history.push("/photographyPortal")
+                }
+                else if(res.data.message === "GeneralBody")
+                {
+                    history.push("/GeneralPortal")
+                }
+                else{
+                    history.push("/studentPortal")
+                }
+            })
+    }
     return (
         <div className="home">
             
@@ -66,12 +106,61 @@ const Try = () => {
                 </video>
 
             </div>
-        {/* <div className="container2"></div>
-        <div className="container3"></div> */}
-            <div className="container2">
-                <img src={cover6} className="d-block w-100 h-50"></img>
-                <button>check</button>
+                {/* <div className="container2"></div>
+                <div className="container3"></div> */}
+            <div className="container3">
+                <div className="signupdiv" >
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <h1>Sign Up Today</h1>
+                    <h4>and see it for yourself!</h4>
+                    </div>
+               
+                <div className="loginHome">
+                    
+                    <div>
+                        <img className="homeimg" src={logo}/>
+                        <br></br>
+                        <h4>Already a Member? Login</h4> 
+                        </div>
+                    <br></br>
+
+
+                        <div className="email">
+                                <label id="mail">Email</label>
+                                <br></br>
+                                {/* <input className="emailtak"></input> */}
+                                <input className="emailtak" type="text" name="email" value={user.email} onChange={handleChange}></input>
+                            </div>
+                    <br></br>
+                    
+                            <div className="pass">
+                                <label >Password</label>
+                                <br></br>
+                                {/* <input className="passcode"></input> */}
+                                <input className="passcode" type="password" name="password" value={user.password}  onChange={handleChange}></input>
+                            </div>    
+                            <br></br>
+
+                        <div>
+                            <button className="btnReg" onClick={login}>Login</button>
+                            </div>
+                        <div><h6>OR</h6></div>
+                        <div><button className="btnlog" onClick={() => history.push("/register")}>Register</button></div>
+                        <br></br>
+                        <div>
+
+                        
+                    </div>
+                </div>
+                {/* <img src={cover6} className="d-block w-100 h-50"></img> */}
             </div>
+            <footer className="mt-2 p-2 text-center text-secondary ">
+                Copyright © 2022 Team Welp FAST CFD. All Rights Reserved.</footer>
         </div>
         
     )
