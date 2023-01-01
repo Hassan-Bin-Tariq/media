b3b9514bf38afcfe171dc6597d4e974369cda99d
tehreem71 <f190143@cfd.nu.edu.pk>
2023-01-02 04:57:59 +0500

    setting up frame-motion carousel

--- src/components/homepage/Try.jsx
+++ src/components/homepage/Try.jsx
@@ -1,33 +1,27 @@
 import React from 'react'
-import { useEffect } from "react";
 import "./Try.css";
 import Carousel from 'react-bootstrap/Carousel';
 import * as THREE from 'three';
 import { color } from '@mui/system';
-import icit from "../../assets/icit.jpg";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import convo1 from "../../assets/convo1.png";
-import crop1 from "../../assets/crop1.png";
-import crop2 from "../../assets/crop2.png";
-import cover1 from "../../assets/cover1.png";
-import cover2 from "../../assets/cover2.png";
-import cover3 from "../../assets/cover3.png";
-import cover4 from "../../assets/cover4.png";
+import {motion} from "framer-motion";
 import vid from "../../assets/vid.mp4";
 import party from "../../assets/party.mp4";
-import cover5 from "../../assets/cover5.png";
-import cover6 from "../../assets/new.png";
-import  { useState } from "react"
+import back from "../../assets/back.png";
+import cover2 from "../../assets/cover2.png";
+import  { useState,useRef,useEffect } from "react"
 import axios from "axios"
 import { useHistory } from "react-router-dom"
 //import Picture1 from './assets/Picture1.png'
 import logo from "../../assets/Picture1.png";
+import Header from '../Header/Header';
 
 const Try = ({ setLoginUser }) => {
     const history = useHistory()
+    const [width,setWidth]=useState(0);
+    const carousel=useRef();
+    useEffect(()=>{
+        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    },[]);
     //asd
     const [user, setUser] = useState({
         email: "",
@@ -67,43 +61,36 @@ const Try = ({ setLoginUser }) => {
                 }
             })
     }
+    
     return (
         <div className="home">
+            <div className="container1">
+                <img src={back} className="d-block w-100 h-100"></img>
+                
+            </div> 
             
-            <Carousel fade>
-                <Carousel.Item interval={3000}>
-                    <img
-                    className="d-block w-100 h-100"
-                    src={convo1}
-                    alt="First slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item interval={3000}>
-                    <img
-                    className="d-block w-100 h-50"
-                    src={crop2}
-                    alt="Second slide"
-                    />
-                </Carousel.Item >
-                <Carousel.Item interval={3000}>
-                    <img
-                    className="d-block w-100 h-50"
-                    src={crop1}
-                    alt="Third slide"
-                    />
-                </Carousel.Item>
-                </Carousel>
             <div className="container2">
-                {/* <img src={cover3} className="d-block w-100 h-50"></img> */}
-                <video className="d-block w-100 h-50" loop="true" autoplay="autoplay" muted>
-                <source src={party} type="video/mp4" ></source>
-                </video> 
+                <motion.div ref={carousel} className="carousel" whileTap={{cursor:"grabbing"}}>
+                    <motion.div drag="x"
+                    dragConstraints={{right:0,left:-width}}
+                     className="inner-carousel">
+                        <motion.div className="item">
+                            <img src={cover2}></img>
+                        </motion.div>
+                        <motion.div className="item">
+                        <img src={back}></img>
+                        </motion.div>
+                        <motion.div className="item">
+                        <img src={cover2}></img>
+                        </motion.div>
+                        <motion.div className="item">
+                        <img src={back}></img>
+                        </motion.div>
+                    </motion.div>
+                </motion.div>
             </div> 
             <div className="container2">
-                {/* <img src={cover4} className="d-block w-100 h-50"></img> */}
-                <video className="d-block w-100 h-50" loop="true" autoplay="autoplay" muted>
-                <source src={vid} type="video/mp4" ></source>
-                </video>
+                
 
             </div>
                 {/* <div className="container2"></div>
