cad55aa1023845dd4fa6fbd84b21ea67b6f1646d
tehreem71 <f190143@cfd.nu.edu.pk>
2023-01-03 17:51:23 +0500

    landing page v1.2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,12 +5,16 @@ import * as THREE from 'three';
 import { color } from '@mui/system';
 import Header from '../Header/Header';
 import Try from '../homepage/Try';
+import Announcement from '../Header/Announcement';
+import Footer from './Footer';
 const Home = () => {
     
   return (
     <>
+    <Announcement/>
     <Header />
     <Try/>
+    <Footer></Footer>
     </>
     
   )
