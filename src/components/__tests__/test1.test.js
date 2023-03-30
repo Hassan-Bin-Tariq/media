2c25469ecbd244f4d04cf083c0edd77f6f8703b2
tehreem71 <f190143@cfd.nu.edu.pk>
2023-03-30 20:37:07 +0500

    testcase2

--- src/components/__tests__/test1.test.js
+++ src/components/__tests__/test1.test.js
@@ -2,7 +2,7 @@ import{render, screen,cleanup} from '@testing-library/react';
 import renderer from 'react-test-render';
 import Login from "./components/login/login"
 
-
+//testing login
 test('should render the foven component', () =>
 {
     render(<Login/>)
@@ -18,4 +18,22 @@ test('matches snapshot', ()=>
     const tree = render.create(<Login/>).toJSON;
     console.log(tree);
     expect((tree).toMatchSnapshot());
+})
+
+//mock
+export const getLogin=() =>
+{
+
+    return;
+}
+const mockInfo = jest.fn();
+const mockWarn = jest.fn();
+// mocking Logging
+jest.mock(<Login/>, () => {
+    return {
+        getLogin: () => ({
+            info: mockInfo,
+            warn: mockWarn
+        })
+    }
 })
