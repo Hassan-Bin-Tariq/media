7b24b3724f5a5a313454584ac8c78b50bc4583da
tehreem71 <f190143@cfd.nu.edu.pk>
2023-03-30 20:42:40 +0500

    testcase pupetter

--- src/components/__tests__/test1.test.js
+++ src/components/__tests__/test1.test.js
@@ -1,6 +1,58 @@
 import{render, screen,cleanup} from '@testing-library/react';
 import renderer from 'react-test-render';
 import Login from "./components/login/login"
+import puppeteer from "puppeteer";
+
+describe("App.js", () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+  });
+
+  it("shows a success message after submitting a form", async () => {
+    await page.goto("http://localhost:3000");
+    await page.waitForSelector(".form-header");
+
+    await page.click(".form-input__email");
+    await page.type(".form-input__email", "username@gmail.com");
+
+    await page.click(".form-input__password");
+    await page.type(".form-input__password", "password");
+
+    await page.click(".form-submit-button");
+
+    await page.waitForSelector(".form-success-message");
+
+    const text = await page.$eval(
+      ".form-success-message",
+      (e) => e.textContent
+    );
+    expect(text).toContain("You are now signed in.");
+  });
+
+  it("shows an error message if authentication fails", async () => {
+    await page.goto("http://localhost:3000");
+    await page.waitForSelector(".form-header");
+
+    await page.click(".form-input__email");
+    await page.type(".form-input__email", "username@gmail.com");
+
+    await page.click(".form-input__password");
+    await page.type(".form-input__password", "password123");
+
+    await page.click(".form-submit-button");
+
+    await page.waitForSelector(".form-error-text");
+
+    const text = await page.$eval(".form-error-text", (e) => e.textContent);
+    expect(text).toContain("Please enter a correct username/password.");
+  });
+
+  afterAll(() => browser.close());
+});
 
 //testing login
 test('should render the foven component', () =>
